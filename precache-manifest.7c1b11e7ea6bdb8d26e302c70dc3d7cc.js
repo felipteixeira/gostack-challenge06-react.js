f327c2822e78f9c5225f3b2124b76a886b0a0f1a
Felipe Teixeira <felipeantonioteixeira@gmail.com>
2020-09-02 11:22:17 -0300

    Updates

---
+++ precache-manifest.7c1b11e7ea6bdb8d26e302c70dc3d7cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b97cf548176806acb1eb324e962aa6",
+    "url": "/gostack-challenge06-react.js/index.html"
+  },
+  {
+    "revision": "c2388c9c5eb4bf1b174c",
+    "url": "/gostack-challenge06-react.js/static/js/2.7dfd8cf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gostack-challenge06-react.js/static/js/2.7dfd8cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a5688d4c48a6bcbed4",
+    "url": "/gostack-challenge06-react.js/static/js/main.41706a28.chunk.js"
+  },
+  {
+    "revision": "84bc727d0b2b2cf2d314",
+    "url": "/gostack-challenge06-react.js/static/js/runtime-main.3e0880e6.js"
+  },
+  {
+    "revision": "78a343ae8fc9abcf44feb04b5656f5fc",
+    "url": "/gostack-challenge06-react.js/static/media/alert.78a343ae.svg"
+  },
+  {
+    "revision": "df4567f063bf4458db532e5ac63df30a",
+    "url": "/gostack-challenge06-react.js/static/media/income.df4567f0.svg"
+  },
+  {
+    "revision": "ce269cac7c70f224e69b361665e6d3e0",
+    "url": "/gostack-challenge06-react.js/static/media/logo.ce269cac.svg"
+  },
+  {
+    "revision": "bb31a5f33ccfe334fa7c72e294ba2984",
+    "url": "/gostack-challenge06-react.js/static/media/outcome.bb31a5f3.svg"
+  },
+  {
+    "revision": "7f99c5ac38306dc0adf301bf383f882f",
+    "url": "/gostack-challenge06-react.js/static/media/total.7f99c5ac.svg"
+  }
+]);
